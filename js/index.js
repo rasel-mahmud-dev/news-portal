b5b769d8a569e4a5ec8ceaa19c3e6138d3d9f01e
rasel mahmud dev <rasel.mahmud.dev@gmail.com>
2022-09-02 23:53:20 +0600

    fetch news and append to html

--- js/index.js
+++ js/index.js
@@ -47,3 +47,92 @@ function handleClickOnCategory(element, categoryId){
 }
 
 
+
+
+function sortByViews(itemA, itemB){
+    if(itemA.total_view > itemB.total_view) {
+        return 1
+    } else  if(itemA.total_view < itemB.total_view) {
+        return 0
+    } else {
+        return -1
+    }
+}
+
+// handle news
+const newsContainer = findById("news-container")
+getNewsByCategoryId(activeCategoryId, (news)=>{
+    if (news){
+
+        newsContainer.innerHTML = null
+
+        news.sort(sortByViews).forEach((eachNews)=>{
+            console.log(eachNews)
+
+            // {name: 'Ukrainska Pravda', published_date: '2022-08-25 18:53:49', img: 'https://images.unsplash.com/photo-1633332755192-72…HxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80'}
+            // others_info {is_todays_pick: false, is_trending: true}
+            // rating {number: 4.5, badge: 'Excellent'}
+
+            const { author, category_id, thumbnail_url, title, total_view, rating, _id, details, image_url, } = eachNews
+
+
+            let newsMarkup  = `
+             <div class="card bg-white shadow-xs p-5">
+                  <div class="flex">
+                       <div class="news-thumb w-full">
+                            <img class="rounded-2xl w-full" src="${thumbnail_url}" alt="news-thumbnail" />
+                        </div>
+                      <div class="card-body p-2 ml-4">
+                            <h2 class="card-title text-neutral-700">${title}</h2>
+                             <p>${details.length > 700 ?   `
+                                    <span>${details.substring(0, 700)}...</span> 
+                                    <button class="btn-link text-blue-400 ml-2">read more</button>
+                                ` : details} 
+                              </p>
+                          
+                              <div class="card-footer flex justify-between mt-4 items-center">
+                                <div class="flex items-center">
+                                    <img src="${(author && author.img) ? author.img : 'images/Avatar.jpg' }" class="w-10 h-10 rounded-full" alt="">
+                                    <div class="ml-1.5">
+                                        <h1 class="text-neutral-500 text-sm font-medium leading-none">${author ? author.name : "Unknown author"}</h1>
+                                        <small class="text-neutral-400">${author ? author.published_date : "unknown date" }</small>
+                                    </div>
+                                </div>
+                                <div class="flex items-center">
+                                    <i class="fa fa-eye"></i>
+                                    <h2 class="ml-1 text-neutral-500 text-sm font-medium">${total_view}</h2>
+                                </div>
+                                <div>
+                                    <div class="rating">
+                                      <input type="radio" name="rating-4" class="w-4 ml-1 mask mask-star-2 bg-blue-500" />
+                                      <input type="radio" name="rating-4" class="w-4 ml-1 mask mask-star-2 bg-blue-500" checked />
+                                      <input type="radio" name="rating-4" class="w-4 ml-1 mask mask-star-2 bg-blue-500" checked />
+                                      <input type="radio" name="rating-4" class="w-4 ml-1 mask mask-star-2 bg-blue-500" />
+                                      <input type="radio" name="rating-4" class="w-4 ml-1 mask mask-star-2 bg-blue-500" />
+                                    </div>
+                                </div>
+                                
+                                <div>
+                                      <i class="fa fa-arrow-right"></i>
+                                </div>
+                            </div>
+                      
+                      </div>
+                  </div>
+			</div>
+             `
+
+            let div = createDomElement("div", "mx-3 p-4", {
+                innerHTML: newsMarkup,
+                events: {
+                    "click": (e)=> alert("hi")
+                }
+            });
+
+            newsContainer.appendChild(div)
+
+        })
+    } else {
+        // handle error
+    }
+})
--- js/utils.js
+++ js/utils.js
@@ -7,3 +7,28 @@ function findById(id){
 
 
 
+/// create daynamacally html element with functions
+
+function createDomElement(tagName="div", className, other){
+    let element = document.createElement(tagName)
+
+    const { innerHTML, innerText, events } = other
+
+    if(className){
+        className.split(" ").map(cls=>{
+            element.classList.add(cls)
+        })
+    }
+    if(innerHTML) element.innerHTML = innerHTML
+    if(innerText) element.innerText = innerText
+
+    if(events && typeof events === "object"){
+        for (const eventsKey in events) {
+            element.addEventListener(eventsKey, events[eventsKey])
+        }
+    }
+
+    return element;
+
+}
+
