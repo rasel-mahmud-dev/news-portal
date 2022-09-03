9dd391942c12bc87df12fadc0467b3e336559dfc
rasel mahmud dev <rasel.mahmud.dev@gmail.com>
2022-09-03 17:42:43 +0600

    added sorting when sort input change

--- js/index.js
+++ js/index.js
@@ -1,16 +1,35 @@
+
+// loader element
+let loader = findById("loading")
+let noNewsMessage = findById("no-news-message")
+
+// category list container
+const categoryList = findById("category-list")
+
+// fetch news slats message container
+const newsCountMessage = findById("news-count-message")
+
+// sort input
+const sortInput = findById("sort")
+
+
+
 // Application State Variables
 let activeCategory = {
     id: "05",
     name: "Entertainment"
 }
 
+let sortBy = {
+    name: "total_view",
+    order: -1, // -1 first show total count of number
+}
 
-// loader element
-let loader = findById("loading")
-let noNewsMessage = findById("no-news-message")
+let fetchedNews = []
 
 
-const categoryList = findById("category-list")
+
+
 
 
 /**
@@ -42,7 +61,7 @@ getAllCategory((categoryData) => {
     }
 })
 
-
+// handle click on each news item
 function handleClickOnCategory(element, categoryId) {
     activeCategory = {
         id: categoryId,
@@ -68,33 +87,42 @@ function handleClickOnCategory(element, categoryId) {
 }
 
 
-function sortByViews(itemA, itemB) {
-    if (itemA.total_view > itemB.total_view) {
-        return 1
-    } else if (itemA.total_view < itemB.total_view) {
-        return 0
-    } else {
-        return -1
-    }
+
+// array sort handler fn
+function sortHandler(a, b) {
+    let order = sortBy.order
+    let property = sortBy.name
+
+
+    let result = (a[property] > b[property]) ? 1 : (a[property] < b[property]) ? -1 : 0;
+
+    return result * order
 }
 
 
-// handle news
 
 // all news container
 const newsContainer = findById("news-container")
 
-// fetch news slats message container
-const newsCountMessage = findById("news-count-message")
 
-
+// fetch all news and render in dom
 function fetchNewsHandler(id) {
-    getNewsByCategoryId(id, (news, errMessage) => {
 
+    newsCountMessage.innerText = ""
+    noNewsMessage.innerText = ""
+
+    getNewsByCategoryId(id, (news, errMessage) => {
         // hide news loader
         loader.classList.add("hidden")
         newsContainer.innerHTML = null
 
+        // set default sort by total_view
+        sortBy = {
+            name: "total_view",
+            order: -1
+        }
+        // set default sort value in select
+        sortInput.value = "total_view"
 
         if (!errMessage) {
 
@@ -104,23 +132,37 @@ function fetchNewsHandler(id) {
                 return;
             }
 
+            // store news in global variable
+            fetchedNews = news
+
             // update response message
             newsCountMessage.innerText = `${news.length} items found for category ${activeCategory.name}`
 
             newsContainer.innerHTML = null
+            createNews(news)
 
-            news.sort(sortByViews).forEach((eachNews) => {
+        } else {
+            // handle error
+            newsCountMessage.innerText = errMessage
+            noNewsMessage.innerText = "No news fetched because internet interrupt"
+        }
+    })
+}
 
-                const {
-                    author,
-                    thumbnail_url,
-                    title,
-                    total_view,
-                    _id,
-                    details,
-                } = eachNews
 
-                let newsMarkup = `
+// loop all news and populated in dom
+function createNews(news){
+    news.sort(sortHandler).forEach((eachNews) => {
+        const {
+            author,
+            thumbnail_url,
+            title,
+            total_view,
+            details,
+            _id,
+        } = eachNews
+
+        let newsMarkup = `
                  <div class="card bg-white shadow-xs p-6 my-8">
                       <div class="flex flex-col md:flex-row">
                            <div class="news-thumb w-full mx-auto md:mx-auto">
@@ -157,7 +199,7 @@ function fetchNewsHandler(id) {
                                     </div>
                                     
                                     <div class="justify-self-end">
-                                          <i class="fa fa-arrow-right"></i>
+                                          <i class="fa fa-arrow-right text-blue-400 cursor-pointer" onclick="fetchNewsDetails('${_id}')"></i>
                                     </div>
                                 </div>
                           
@@ -166,25 +208,43 @@ function fetchNewsHandler(id) {
                 </div>
              `
 
-                let div = createDomElement("div", "", {
-                    innerHTML: newsMarkup,
-                    events: {
-                        "click": () => fetchNewsDetails(_id)
-                    }
-                });
+        let div = createDomElement("div", "", {
+            innerHTML: newsMarkup,
+            events: {
+                "click": () => fetchNewsDetails(_id)
+            }
+        });
 
-                newsContainer.appendChild(div)
+        newsContainer.appendChild(div)
 
-            })
-        } else {
-            // handle error
-            newsCountMessage.innerText = errMessage
-            noNewsMessage.innerText = "No news fetched because internet interrupt"
-        }
     })
 }
 
+
 fetchNewsHandler(activeCategory.id)
+
+
+// change sort value event listener
+sortInput.addEventListener("change", (e)=>{
+    if(e.target.value === "total_view"){
+        sortBy = {
+            name: "total_view",
+            order: -1
+        }
+    } else if(e.target.value === "rating") {
+        sortBy = {
+            name: "rating.number",
+            order: -1
+        }
+    } else if(e.target.value === "title") {
+        sortBy = {
+            name: "title",
+            order: 1
+        }
+    }
+    newsContainer.innerHTML = null
+    createNews(fetchedNews)
+})
 
 
 
@@ -201,59 +261,79 @@ async function fetchNewsDetails(newsId) {
         let res = await fetch(`https://openapi.programming-hero.com/api/news/${newsId}`)
         let data = await res.json()
         if (!data.status) {
+            modalContent.innerHTML = `
+             <div>
+                <h1 class="text-2xl text-center mt-4 font-medium">No news found with give id ${newsId}</h1> 
+            </div>
+           `
             return;
         }
 
+        const {author, others_info,  title, total_view, rating, details, image_url} = data.data[0]
 
-        data.data.forEach((news) => {
-            const {author, category_id, thumbnail_url, title, total_view, rating, _id, details, image_url,} = news
-            const t = `
-                 <div>
-                    <div class="flex justify-between items-center">
-                        <div>
-                            <div class="flex items-center">
-                                <img class="w-7 rounded-full h-7" src="${author.img}" alt="">
-                                <h1 class="ml-2">${author.name}</h1>
-                            </div>
-                            <h1 class="text-2xl text-neutral-900 font-medium">${title}</h1>
+        modalContent.innerHTML = `
+             <div>
+                <div class="flex justify-between items-center">
+                    <div>
+                        <div class="flex items-center">
+                            <img class="w-7 rounded-full h-7" src="${author && author.img && author.img}}" alt="">
+                            <h1 class="ml-2">${author && author.name ? author.name : "no author name found"}</h1>
                         </div>
-                        <button class="btn bg-blue-600" onclick="closeModal()">
-                            <i class="fa fa-times"></i>
-                        </button>
-            
+                        <h1 class="text-2xl mt-2 text-neutral-900 font-medium">${title}</h1>
+                    </div>
+                    <button class="btn bg-blue-600" onclick="closeModal()">
+                        <i class="fa fa-times"></i>
+                    </button>
+        
+                </div>
+        
+                <div class="flex gap-x-4 my-4 items-center flex-wrap gap-2">
+                    <div class="category-item active-category">
+                        ${activeCategory.name}
                     </div>
             
-                    <div class="flex gap-x-4 my-4 items-center">
-                        <span>
-                            <div class="flex bg-blue-500 px-4 py-1 items-center text-white rounded">
-                                <span class="text-md">${rating.number}</span>
-                                <i class="fa fa-star ml-1 -mt-1 text-sm"></i>
-                            </div>
-                        </span>
-                        <span>
-                            <i class="fa fa-eye"></i>
-                            <span>${total_view}</span>
-                        </span>
-            
-                        <div>
-                            <i class="fa fa-clock"></i>
-                            <span class="text-neutral-400 font-medium">${"author"}</span>
+                    
+                    ${others_info && others_info.is_trending ? `
+                        <div class="category-item active-category">Trending</div>
+                    ` : ""}
+                    
+                </div>
+                
+                <div class="flex gap-x-4 my-4 items-center flex-wrap gap-2">
+                    <span>
+                        <div class="flex bg-blue-500 px-4 py-1 items-center text-white rounded">
+                            <span class="text-md">${rating.number}</span>
+                            <i class="fa fa-star ml-1 -mt-1 text-sm"></i>
                         </div>
+                    </span>
+                    <span>
+                        <i class="fa fa-eye"></i>
+                        <span>${total_view ? total_view : "No Data found "}</span>
+                    </span>
+        
+                    <div>
+                        <i class="far fa-clock"></i>
+                        <span class="text-neutral-400 font-medium">${author.published_date}</span>
                     </div>
-            
-                    <div class="w-full">
-                        <img class="w-full" src="${image_url}" alt="">
-                    </div>
-            
-                    <p class="mt-4">${details}</p>
-                </div>           
-                             
-            `
-            modalContent.innerHTML = t
-        })
+                </div>
+        
+                <div class="w-full">
+                    <img class="w-full" src="${image_url}" alt="">
+                </div>
+        
+                <p class="mt-4">${details}</p>
+            </div>           
+                         
+        `
+
 
     } catch (ex) {
-
+        modalContent.innerHTML = `
+             <div>
+                <h1 class="text-2xl text-center mt-4 font-medium">No news found with give id ${newsId}</h1> 
+            </div>
+        `
     }
 
 }
+
